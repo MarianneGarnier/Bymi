5fc26610bf22d7452e8382749cd9addc96483998
RonanDefours <ronan@outlook.fr>
2019-09-12 11:01:37 +0200

    initial frontend commit

---
+++ src/main/webapp/app/component/list-display-product/list-display-product.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'app/shared/model/product.model';
+
+@Component({
+  selector: 'jhi-list-display-product',
+  templateUrl: './list-display-product.component.html',
+  styleUrls: ['./list-display-product.component.scss']
+})
+export class ListDisplayProductComponent implements OnInit {
+  //TODO: passer cette liste en input et créer un service pour charger une liste de produits depuis le back-end
+  products: Product[];
+
+  constructor() {}
+
+  ngOnInit() {
+    let product1 = new Product(1, 1, 'test01', 15, 'null', 2, null, null);
+    let product2 = new Product(1, 2, 'test01', 16, 'null', 2, null, null);
+    let product3 = new Product(1, 3, 'test01', 95, 'null', 1, null, null);
+    this.products = [];
+    this.products.push(product1);
+    this.products.push(product2);
+    this.products.push(product3);
+  }
+}
---
+++ src/main/webapp/app/single-display-product/single-display-product.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-single-display-product',
+  templateUrl: './single-display-product.component.html',
+  styleUrls: ['./single-display-product.component.scss']
+})
+export class SingleDisplayProductComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
