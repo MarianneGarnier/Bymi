09bc45b51f1d0c0b330a0691af493da047092ee3
MaximeDeus <maxime.deroissart@etu.univ-grenoble-alpes.fr>
2019-09-18 22:43:59 +0200

    MàJ logique affichage des types de paiement (ngfor), test CI

--- src/main/webapp/app/order/make-order/make-order.component.ts
+++ src/main/webapp/app/order/make-order/make-order.component.ts
@@ -16,6 +16,7 @@ export class MakeOrderComponent implements OnInit {
   account: Account;
   user: User;
   userAuthenticated = false;
+  payments = ['VISA', 'MASTERCARD', 'GB'];
   months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   years = [2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030];
 
