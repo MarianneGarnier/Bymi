34796d49db6837f8e4c6945ee501ef3d3d1ca5f9
Marianne <marianne.garnier@etu.univ-grenoble-alpes.fr>
2019-09-19 17:17:53 +0200

    Création formulaire ajout produit

--- src/main/webapp/app/selling/create-product/create-product.module.ts
+++ src/main/webapp/app/selling/create-product/create-product.module.ts
@@ -3,10 +3,12 @@ import { RouterModule } from '@angular/router';
 import { CREATE_PRODUCT_ROUTE } from 'app/selling/create-product/create-product.route';
 import { CreateProductComponent } from 'app/selling/create-product/create-product.component';
 import { BymiSharedModule } from 'app/shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [BymiSharedModule, RouterModule.forChild([CREATE_PRODUCT_ROUTE])],
+  imports: [BymiSharedModule, RouterModule.forChild([CREATE_PRODUCT_ROUTE]), FormsModule, ReactiveFormsModule],
   declarations: [CreateProductComponent],
+  exports: [CreateProductComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CreateProductModule {}
