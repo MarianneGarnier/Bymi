23f09fc5822938271854b035d4fea6ee3cbf512c
Marianne <marianne.garnier@etu.univ-grenoble-alpes.fr>
2019-09-20 09:52:14 +0200

    Ajout lien vers profil

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -40,7 +40,8 @@ import {
   faAsterisk,
   faTasks,
   faHome,
-  faShoppingBag
+  faShoppingBag,
+  faUserCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 // Adds the SVG icon to the library so you can use it in your page
@@ -79,5 +80,6 @@ library.add(faSearch);
 library.add(faTrashAlt);
 library.add(faAsterisk);
 library.add(faShoppingBag);
+library.add(faUserCircle);
 
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
