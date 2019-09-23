d73759aa252ef83b8f6303b03255c45af7015479
RonanDefours <ronan.defours@etu.univ-grenoble-alpes.fr>
2019-09-23 08:50:03 +0200

    modifs front

--- src/main/webapp/app/component/profile/profile.component.ts
+++ src/main/webapp/app/component/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../core';
 
 @Component({
   selector: 'jhi-profil',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  user: User;
+  date: String;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user = new User(
+      1,
+      'sacha56',
+      'Sacha',
+      'Cargenl',
+      'sacha@outlook.fr',
+      null,
+      null,
+      null,
+      null,
+      new Date(2002, 5, 12),
+      null,
+      null,
+      '1234'
+    );
+    let tempDate = this.user.createdDate;
+    this.date = tempDate.getDay() + '/' + tempDate.getMonth() + '/' + tempDate.getFullYear();
+  }
 }
