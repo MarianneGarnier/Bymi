0d09ee795c5a80471e62646a7e11489880186032
geoliak <geoliak@users.noreply.github.com>
2019-09-27 01:12:02 +0200

    Bouton ajouter au panier marche depuis l'ecran d'accueil

--- src/main/webapp/app/component/small-display-product/small-display-product.component.ts
+++ src/main/webapp/app/component/small-display-product/small-display-product.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Product } from '../../shared/model/product.model';
+import { Product, IProduct } from '../../shared/model/product.model';
+import { PlacedOrder, OrderStatus } from 'app/shared/model/placed-order.model';
+import { OrderLine, OrderLineStatus } from 'app/shared/model/order-line.model';
+import { HttpResponse } from '@angular/common/http';
+import { SearchService } from 'app/search/search.service';
+import { User, IUser } from 'app/core/user/user.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'jhi-small-display-product',
@@ -9,9 +15,39 @@ import { Product } from '../../shared/model/product.model';
 export class SmallDisplayProductComponent implements OnInit {
   @Input() public product: Product;
 
-  constructor() {}
+  constructor(public search: SearchService) {}
 
   ngOnInit() {}
 
-  public addBasket() {}
+  async addProductFromListToBasket() {
+    let productFromdDB: Product;
+    let orders: PlacedOrder[];
+    let basket: PlacedOrder = null;
+    let orderLineToAdd: OrderLine = null;
+    const currentUser: User = await this.search.getCurrentUser().then((res: HttpResponse<IUser>) => res.body);
+
+    await this.search.findProductById(this.product.id).then((res: HttpResponse<IProduct>) => {
+      productFromdDB = res.body;
+    });
+    if (productFromdDB.quantity > 0) {
+      await this.search.findOrdersByUser().then((res: HttpResponse<PlacedOrder[]>) => (orders = res.body));
+
+      orders.forEach(order => {
+        if (order.status === OrderStatus.BASKET) {
+          basket = order;
+        }
+      });
+
+      if (basket === null) {
+        basket = new PlacedOrder(undefined, moment(), Math.round(Math.random() * 10000), OrderStatus.BASKET, undefined, currentUser);
+        await this.search.createPlacedOrder(basket).then((res: HttpResponse<PlacedOrder>) => (basket = res.body));
+      }
+
+      orderLineToAdd = new OrderLine(undefined, 1, moment(), OrderLineStatus.RESERVED, this.product, basket);
+      await this.search.createOrderLine(orderLineToAdd);
+
+      productFromdDB.quantity = productFromdDB.quantity - 1;
+      this.search.updateProduct(productFromdDB);
+    }
+  }
 }
